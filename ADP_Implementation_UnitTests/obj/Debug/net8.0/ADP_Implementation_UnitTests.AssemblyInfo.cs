2177b0b3b50d4f51db6d8ea0af899f0b20822f91
Boy Thach <boy_thach@hotmail.com>
2024-12-16 23:13:57 +0100

    Remove unused variable in Deque benchmark

--- ADP_Implementation_UnitTests/obj/Debug/net8.0/ADP_Implementation_UnitTests.AssemblyInfo.cs
+++ ADP_Implementation_UnitTests/obj/Debug/net8.0/ADP_Implementation_UnitTests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ADP_Implementation_UnitTests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1daf28457483e599b4c61ba26433b21ea8297d3a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4dd443e2c6164d260dc8365fb21463c115eb5ff2")]
 [assembly: System.Reflection.AssemblyProductAttribute("ADP_Implementation_UnitTests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ADP_Implementation_UnitTests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
